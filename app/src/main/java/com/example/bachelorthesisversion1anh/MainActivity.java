2b1929b5c1c2fee44384e806d686cb92f19f33c8
unknown <ahunghpvn@gmail.com>
2020-04-09 11:36:38 +0200

    my bachelor thesis project

---
+++ app/src/main/java/com/example/bachelorthesisversion1anh/MainActivity.java
@@ -0,0 +1,93 @@
+package com.example.bachelorthesisversion1anh;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.RadioButton;
+import android.widget.RadioGroup;
+import android.widget.Toast;
+
+public class MainActivity extends AppCompatActivity {
+    RadioGroup radioSelection, radioView;
+    EditText numberOfElement;
+    Button generateButton;
+    int tmp1=0, tmp2=0;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        radioSelection = (RadioGroup) findViewById(R.id.selectGroup);
+        radioView = (RadioGroup) findViewById(R.id.viewGroup);
+        numberOfElement = (EditText) findViewById(R.id.editText);
+        generateButton = (Button) findViewById(R.id.button);
+
+        //code select alogrithms
+        radioSelection.setOnCheckedChangeListener(new RadioGroup.OnCheckedChangeListener() {
+            @Override
+            public void onCheckedChanged(RadioGroup group, int checkedId) {
+                //id from button
+                switch (checkedId){
+                    case R.id.quickSort:
+                        tmp1 = 1;
+                        break;
+                    case  R.id.selectGroup:
+                        tmp1 = 2;
+                        break;
+                    case R.id.mergeSort:
+                        tmp1 = 3;
+                        break;
+                    case R.id.bubbleSort:
+                        tmp1 = 4;
+                        break;
+                    case R.id.insertSort:
+                        tmp1 = 5;
+                        break;
+                }
+            }
+        });
+        // code View
+        radioView.setOnCheckedChangeListener(new RadioGroup.OnCheckedChangeListener() {
+            @Override
+            public void onCheckedChanged(RadioGroup group, int checkedId) {
+                switch (checkedId){
+                    case R.id.portrait:
+                       tmp2 = 1;
+                        break;
+                    case R.id.landscape:
+                        tmp2=2;
+                        break;
+                }
+            }
+        });
+        //BUTTON
+        generateButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String numberElement = numberOfElement.getText().toString().trim();
+                if(numberElement.isEmpty()){
+                    Toast.makeText(MainActivity.this,"Please insert the number elements",Toast.LENGTH_SHORT).show();
+                }if(tmp1==0){
+                    Toast.makeText(MainActivity.this,"Please select sorting alogrithm",Toast.LENGTH_SHORT).show();
+                }if(tmp2==0){
+                    Toast.makeText(MainActivity.this,"Please select View",Toast.LENGTH_SHORT).show();
+                }
+                if(numberElement.isEmpty()==false&&tmp1!=0&&tmp2!=0){
+                    int number = Integer.parseInt(numberElement);
+                    if(tmp2==1){
+                        Intent intent = new Intent(MainActivity.this, portaitView.class);
+                        startActivity(intent);
+                    }
+                    else{
+                        Intent intent = new Intent(MainActivity.this, landscapeView.class);
+                        startActivity(intent);
+                    }
+                }
+
+            }
+        });
+    }
+}
---
+++ app/src/main/java/com/example/bachelorthesisversion1anh/landscapeView.java
@@ -0,0 +1,16 @@
+package com.example.bachelorthesisversion1anh;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.pm.ActivityInfo;
+import android.os.Bundle;
+
+public class landscapeView extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_landscape_view);
+
+    }
+}
